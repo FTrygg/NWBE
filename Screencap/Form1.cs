c8f3f43971bca674575b88ab4f153a37d72c30ee
FT <finn.jonas@gmx.de>
2023-05-16 22:02:42 +0100

    Implement cursor move and screenshot

--- Screencap/Form1.cs
+++ Screencap/Form1.cs
@@ -5,6 +5,7 @@ using System.Drawing;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Windows.Forms;
+using System.Reflection.Metadata;
 
 namespace Screencap
 {
@@ -23,6 +24,13 @@ namespace Screencap
 
         [DllImport("user32.dll")]
         private static extern IntPtr GetWindowRect(IntPtr hWnd, ref Rect rect);
+
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(uint dwFlags, uint dx, uint dy, uint cButtons, uint dwExtraInfo);
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02;
+        private const int MOUSEEVENTF_LEFTUP = 0x04;
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08;
+        private const int MOUSEEVENTF_RIGHTUP = 0x10;
 
         [StructLayout(LayoutKind.Sequential)]
         private struct Rect
@@ -49,39 +57,48 @@ namespace Screencap
         
         private void SwitchApp(apps app)
         {
-            string appname = (app == apps.Newworld) ? "Newworld.exe" : "NWGE.exe";
+            string appname = (app == apps.Newworld) ? "NewWorld" : "NWGE.exe";
 
-            Process[] procs = Process.GetProcesses();
-            if (procs.Length != 0)
+            Process[] processes = Process.GetProcessesByName(appname);
+            if (processes.Length == 1 )
             {
-                for (int i = 0; i < procs.Length; i++)
-                {   
-                    try
-                    {
-                        if (procs[i].MainModule.ModuleName == "CiscoCollabHost.exe")
-                        {
-                            IntPtr hwnd = procs[i].MainWindowHandle;
-                            ShowWindowAsync(hwnd, SW_RESTORE);
-                            SetForegroundWindow(hwnd);
-                            Bitmap pic = Cap(GetForegroundWindow());
-                            ImageSave("webextestpic",ImageFormat.Png,pic);
-                            return;
-                        }
-                        else
-                        {
-                            //Debug.WriteLine(procs[i].MainModule.ModuleName);
-                        }
-                    }
-                    catch
-                    {
-                    }
-                }
+                Debug.WriteLine("Found process");
+                IntPtr hwnd = processes[0].MainWindowHandle;
+                ShowWindowAsync(hwnd, SW_RESTORE);
+                SetForegroundWindow(hwnd);
+                Debug.WriteLine($"Set process to {appname}");
+
+
+                SendKeys.SendWait("{ESC}");
+                Thread.Sleep(700);
+                SendKeys.SendWait("{TAB}");
+                Thread.Sleep(500);
+
+                Rectangle bounds;
+                var rect = new Rect();
+                GetWindowRect(hwnd, ref rect);
+                bounds = new Rectangle(rect.Left, rect.Top, rect.Right - rect.Left, rect.Bottom - rect.Top);
+                
+                Cursor = new Cursor(hwnd);
+                Cursor.Position = new Point(360,996);
+                Cursor.Clip = bounds;
+                
+                Thread.Sleep(500);
+
+                uint X = (uint)Cursor.Position.X;
+                uint Y = (uint)Cursor.Position.Y;
+                mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 0, 0);
+
+                Cursor.Position = new Point(360, 996);
+                Thread.Sleep(500);
+                ImageSave("",ImageFormat.Png,Cap(hwnd));
+
             }
-            else
-            { 
-                return;
-            }
-            MessageBox.Show("New World does not seem to be running...");
+        }
+        private void createGearsetOverview(IntPtr hwnd)
+        {
+            
+            //Cursor.Clip = new Rectangle(this.Location, this.Size);
         }
 
         public Bitmap Cap(IntPtr handle)
@@ -102,7 +119,7 @@ namespace Screencap
         static void ImageSave(string filename, ImageFormat format, Image image)
         {
             format = format ?? ImageFormat.Png;
-            filename = @"C:\Users\Finn Tryggvason\Desktop\testpic.png";
+            filename = @"C:\Users\Finn\Desktop\testpic.png";
             image.Save(filename, format);
         }
 
